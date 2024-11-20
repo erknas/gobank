bdf11927990057713a7fc6e1cd2eeb24e40c67e8
Aleksandr <vin89441@gmail.com>
2024-11-20 18:25:17 +0300

    added constraint for negative balance

--- migrations/20241108095448_new_accounts_table.sql
+++ migrations/20241108095448_new_accounts_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE accounts (
 	account_id SERIAL PRIMARY KEY,	
 	user_id INT NOT NULL,
 	number VARCHAR(19) NOT NULL UNIQUE,
-	balance NUMERIC(15,2) DEFAULT 0.00,
+	balance NUMERIC(15,2) DEFAULT 0.00 CHECK (balance >= 0),
 	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE 
 );
 -- +goose StatementEnd
