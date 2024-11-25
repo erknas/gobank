0bf6518c14869680bdfe7abe58ff9e9a0bafaf7d
Aleksandr <vin89441@gmail.com>
2024-11-25 16:05:50 +0300

    added Delete method

--- storer.go
+++ storer.go
@@ -3,9 +3,10 @@ package main
 import "context"
 
 type Storer interface {
-	Register(context.Context, *User) error
-	Charge(context.Context, *ChargeRequest) error
-	Transfer(context.Context, *TransferRequest) error
+	Register(context.Context, *User) (int, error)
+	Charge(context.Context, *ChargeRequest) (float64, error)
+	Transfer(context.Context, *TransferRequest) (float64, error)
 	GetUserByID(context.Context, int) (*User, error)
+	Delete(context.Context, int) error
 	GetUsers(context.Context) ([]*User, error)
 }
