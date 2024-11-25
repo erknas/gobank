2b4161a40a9b5e92b2aa909b4e5c20b23bb39244
Aleksandr <vin89441@gmail.com>
2024-11-25 16:08:52 +0300

    added Delete method and internal errors, update responses

--- api.go
+++ api.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
 
@@ -15,25 +14,36 @@ func (s *Server) handleRegister(ctx context.Context, w http.ResponseWriter, r *h
 	req := new(RegisterUserRequest)
 
 	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
-		return err
+		return InvalidJSON()
 	}
 	defer r.Body.Close()
+
+	if errors := req.ValidateUserData(); len(errors) > 0 {
+		return InvalidRequestData(errors)
+	}
 
 	user, err := NewUser(req.FirstName, req.LastName, req.Email, req.PhoneNumber, req.Password)
 	if err != nil {
 		return err
 	}
 
-	if err := s.store.Register(ctx, user); err != nil {
+	id, err := s.store.Register(ctx, user)
+	if err != nil {
 		return err
 	}
 
 	resp := RegisterUserResponse{
-		Msg:         "user successfully registered",
-		FirstName:   user.FirstName,
-		Email:       user.Email,
-		PhoneNumber: user.PhoneNumber,
-		Balance:     user.Balance,
+		Msg: "user successfully registered",
+		User: User{
+			ID:          id,
+			FirstName:   req.FirstName,
+			LastName:    req.LastName,
+			Email:       req.Email,
+			PhoneNumber: req.PhoneNumber,
+			Number:      user.Number,
+			Balance:     user.Balance,
+			CreatedAt:   user.CreatedAt,
+		},
 	}
 
 	return writeJSON(w, http.StatusOK, resp)
@@ -43,15 +53,24 @@ func (s *Server) handleCharge(ctx context.Context, w http.ResponseWriter, r *htt
 	req := new(ChargeRequest)
 
 	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
-		return err
+		return InvalidJSON()
 	}
 	defer r.Body.Close()
 
-	if err := s.store.Charge(ctx, req); err != nil {
+	if errors := req.ValidateChargeData(); len(errors) > 0 {
+		return InvalidRequestData(errors)
+	}
+
+	balance, err := s.store.Charge(ctx, req)
+	if err != nil {
 		return err
 	}
 
-	resp := ChargeResponse{Msg: "success", Amount: req.Amount}
+	resp := ChargeResponse{
+		Msg:     "success",
+		Amount:  req.Amount,
+		Balance: balance,
+	}
 
 	return writeJSON(w, http.StatusOK, resp)
 }
@@ -60,15 +79,24 @@ func (s *Server) handleTransfer(ctx context.Context, w http.ResponseWriter, r *h
 	req := new(TransferRequest)
 
 	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
-		return err
+		return InvalidJSON()
 	}
 	defer r.Body.Close()
 
-	if err := s.store.Transfer(ctx, req); err != nil {
+	if errors := req.ValidateTransferData(); len(errors) > 0 {
+		return InvalidRequestData(errors)
+	}
+
+	balance, err := s.store.Transfer(ctx, req)
+	if err != nil {
 		return err
 	}
 
-	resp := TransferResponse{Msg: "successful transaction", Amount: req.Amount}
+	resp := TransferResponse{
+		Msg:     "successful transaction",
+		Amount:  req.Amount,
+		Balance: balance,
+	}
 
 	return writeJSON(w, http.StatusOK, resp)
 }
@@ -76,7 +104,7 @@ func (s *Server) handleTransfer(ctx context.Context, w http.ResponseWriter, r *h
 func (s *Server) handleGetUserByID(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	id, err := parseID(r)
 	if err != nil {
-		return err
+		return InvalidID()
 	}
 
 	user, err := s.store.GetUserByID(ctx, id)
@@ -98,14 +126,34 @@ func (s *Server) handleGetUsers(ctx context.Context, w http.ResponseWriter, r *h
 	return writeJSON(w, http.StatusOK, resp)
 }
 
+func (s *Server) handleDelete(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	id, err := parseID(r)
+	if err != nil {
+		return InvalidID()
+	}
+
+	if err := s.store.Delete(ctx, id); err != nil {
+		return err
+	}
+
+	return writeJSON(w, http.StatusOK, nil)
+}
+
 type APIFunc func(context.Context, http.ResponseWriter, *http.Request) error
 
 func makeHTTPFunc(fn APIFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.Background()
-		ctx = context.WithValue(ctx, RequestID{}, uuid.New().String())
+		ctx := context.WithValue(context.Background(), RequestID{}, uuid.New().String())
 		if err := fn(ctx, w, r); err != nil {
-			writeJSON(w, http.StatusBadRequest, nil)
+			if apiErr, ok := err.(APIError); ok {
+				writeJSON(w, apiErr.StatusCode, apiErr)
+			} else {
+				errResp := map[string]any{
+					"statusCode": http.StatusInternalServerError,
+					"msg":        "internal server error",
+				}
+				writeJSON(w, http.StatusInternalServerError, errResp)
+			}
 		}
 	}
 }
@@ -118,11 +166,5 @@ func writeJSON(w http.ResponseWriter, s int, v any) error {
 
 func parseID(r *http.Request) (int, error) {
 	strID := chi.URLParam(r, "id")
-
-	id, err := strconv.Atoi(strID)
-	if err != nil {
-		return -1, fmt.Errorf("invalid id: %s", strID)
-	}
-
-	return id, nil
+	return strconv.Atoi(strID)
 }
--- server.go
+++ server.go
@@ -22,11 +22,12 @@ func NewServer(listenAddr string, store Storer) *Server {
 func (s *Server) Run() error {
 	router := chi.NewRouter()
 
-	router.Post("/register", makeHTTPFunc(s.handleRegister))
+	router.Post("/user", makeHTTPFunc(s.handleRegister))
 	router.Post("/charge", makeHTTPFunc(s.handleCharge))
 	router.Post("/transfer", makeHTTPFunc(s.handleTransfer))
 	router.Get("/user/{id}", makeHTTPFunc(s.handleGetUserByID))
 	router.Get("/users", makeHTTPFunc(s.handleGetUsers))
+	router.Delete("/user/{id}", makeHTTPFunc(s.handleDelete))
 
 	log.Printf("server runing on [http://localhost%s]\n", s.listenAddr)
 
