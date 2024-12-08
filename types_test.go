989e65ec4b920ea1467be556e2c5f3d72bb9f312
Aleksandr <vin89441@gmail.com>
2024-12-08 16:37:27 +0300

    add table test

--- types_test.go
+++ types_test.go
@@ -34,9 +34,9 @@ func TestNewUser(t *testing.T) {
 	firstName := gofakeit.Name()
 	lastName := gofakeit.LastName()
 	phoneNumber := gofakeit.Phone()
-	password := gofakeit.Password(true, true, true, false, false, 10)
+	password := randomFakePassword()
 
-	user, err := NewUser(firstName, lastName, phoneNumber, password)
+	user, err := NewUser(&NewUserRequest{FirstName: firstName, LastName: lastName, PhoneNumber: phoneNumber, Password: password})
 	require.NoError(t, err)
 
 	now := time.Now().UTC()
@@ -52,4 +52,116 @@ func TestNewUser(t *testing.T) {
 
 	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
 	require.NoError(t, err)
+}
+
+func TestNewUser_Invalid(t *testing.T) {
+	tests := []struct {
+		name        string
+		user        NewUserRequest
+		expectedErr string
+	}{
+		{
+			name: "Empty FirstName",
+			user: NewUserRequest{
+				FirstName:   "",
+				LastName:    gofakeit.LastName(),
+				PhoneNumber: gofakeit.Phone(),
+				Password:    randomFakePassword(),
+			},
+			expectedErr: "invalid user data",
+		},
+		{
+			name: "Empty LastName",
+			user: NewUserRequest{
+				FirstName:   gofakeit.FirstName(),
+				LastName:    "",
+				PhoneNumber: gofakeit.Phone(),
+				Password:    randomFakePassword(),
+			},
+			expectedErr: "invalid user data",
+		},
+		{
+			name: "Empty PhoneNumber",
+			user: NewUserRequest{
+				FirstName:   gofakeit.FirstName(),
+				LastName:    gofakeit.LastName(),
+				PhoneNumber: "",
+				Password:    randomFakePassword(),
+			},
+			expectedErr: "invalid user data",
+		},
+		{
+			name: "Empty Password",
+			user: NewUserRequest{
+				FirstName:   gofakeit.FirstName(),
+				LastName:    gofakeit.LastName(),
+				PhoneNumber: gofakeit.Phone(),
+				Password:    "",
+			},
+			expectedErr: "invalid user data",
+		},
+		{
+			name: "Empty all",
+			user: NewUserRequest{
+				FirstName:   "",
+				LastName:    "",
+				PhoneNumber: "",
+				Password:    "",
+			},
+			expectedErr: "invalid user data",
+		},
+		{
+			name: "Invalid PhoneNumber length",
+			user: NewUserRequest{
+				FirstName:   gofakeit.FirstName(),
+				LastName:    gofakeit.LastName(),
+				PhoneNumber: "897651",
+				Password:    randomFakePassword(),
+			},
+			expectedErr: "invalid user data",
+		},
+		{
+			name: "Invalid PhoneNumber contains letters",
+			user: NewUserRequest{
+				FirstName:   gofakeit.FirstName(),
+				LastName:    gofakeit.LastName(),
+				PhoneNumber: "m89u7699a1",
+				Password:    randomFakePassword(),
+			},
+			expectedErr: "invalid user data",
+		},
+		{
+			name: "Invalid PhoneNumber contains symbols",
+			user: NewUserRequest{
+				FirstName:   gofakeit.FirstName(),
+				LastName:    gofakeit.LastName(),
+				PhoneNumber: "8@#$%^&*4)",
+				Password:    randomFakePassword(),
+			},
+			expectedErr: "invalid user data",
+		},
+		{
+			name: "Invalid PhoneNumber contains letters, symbols, digits",
+			user: NewUserRequest{
+				FirstName:   gofakeit.FirstName(),
+				LastName:    gofakeit.LastName(),
+				PhoneNumber: "910-a4 j/3",
+				Password:    randomFakePassword(),
+			},
+			expectedErr: "invalid user data",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			user, err := NewUser(&tt.user)
+			require.Error(t, err)
+			require.Empty(t, user)
+			require.Contains(t, err.Error(), tt.expectedErr)
+		})
+	}
+}
+
+func randomFakePassword() string {
+	return gofakeit.Password(true, true, true, true, false, 10)
 }
